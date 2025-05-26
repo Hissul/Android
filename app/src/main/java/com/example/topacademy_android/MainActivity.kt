4a32595082a82b6243a0bf7700b1c95a287077bb
Alexander <121170207+Hissul@users.noreply.github.com>
2025-05-26 13:05:03 +0300

    hw_5

--- app/src/main/java/com/example/topacademy_android/MainActivity.kt
+++ app/src/main/java/com/example/topacademy_android/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
             validateInput()
         }
     }
+
 
     private fun validateInput() {
         val email = binding.emailInput.text.toString()
@@ -63,5 +65,6 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
 
 }
